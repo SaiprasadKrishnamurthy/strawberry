a2e893800aa142fedeedd78f556047f674646eca
saipkri <saiprkri@cisco.com>
2016-11-12 17:30:15 +0530

    config passed in as part of the payload making the engine flexible

--- engine/src/main/java/HazelConsumer.java
+++ engine/src/main/java/HazelConsumer.java
@@ -3,8 +3,7 @@ import com.strawberry.engine.config.StrawberryConfigHolder;
 public class HazelConsumer {
     public static void main(String[] args) throws Exception {
         StrawberryConfigHolder.init("/Users/saipkri/learning/strawberry/engine/src/main/resources/application.properties");
-        StrawberryConfigHolder.initMongo();
-        StrawberryConfigHolder.hazelcastInstance().getTopic("vehicle-camera-sensor-events").addMessageListener(message -> System.out.println(message.getMessageObject()));
+        StrawberryConfigHolder.hazelcastInstance().getTopic("vehicle-camera-sensor-stream-nh45").addMessageListener(message -> System.out.println(message.getMessageObject()));
     }
 }
 
