a35628eac58efc8bcafad57f7a59d995637a7a64
saipkri <saiprkri@cisco.com>
2016-11-16 15:39:46 +0530

    Readme with Prerequisites added

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-
-## Will be added soon.... :-) ##
 ```
 
                       \VW/
@@ -17,6 +15,34 @@
 
 
 ```
+## PREREQUISITES (FOR A DEV SETUP)##
+* Java 8
+* Maven 3
+* MongoDB v 3.2.1
+* Elasticsearch v 2.4.0
+* Zookeeper v 3.4.9
+* Apache Kafka 2.11-0.9.0.1
+* Apache Storm 1.0.2
+* Kibana 4.6.1
+
+
+## Steps to start (high level steps) ##
+* Start MongoDB
+* Start Zookeeper
+* Start Kafka pointing to the Zookeeper
+* Create a new Database in mongo called strawberry (Use RoboMongo tool for this, quite easy to use)
+* Open the REST project in your IDE (eg: Eclipse)
+* Run Main.java in your eclise as a Java Application.
+* Try to access http://localhost:9090/swagger-ui.html#/  (You should see the swagger UI).
+* You have successfully set up the REST by now.
+
+
+## Kafka handy commands ##
+```
+./kafka-topics.sh --create --topic vehicle-camera-sensor-stream --replication-factor 1 --partitions 3 --zookeeper localhost:2181
+```
+
+
 
 
 
---
+++ engine/src/main/java/com/strawberry/engine/bolts/StatisticalOperationsBolt.java
@@ -0,0 +1,62 @@
+package com.strawberry.engine.bolts;
+
+import com.sai.strawberry.api.DataTransformer;
+import com.sai.strawberry.api.EventStreamConfig;
+import com.strawberry.engine.config.StrawberryConfigHolder;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by saipkri on 18/08/16.
+ */
+public class StatisticalOperationsBolt extends BaseRichBolt {
+    private OutputCollector outputCollector;
+    private int boltId;
+
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+        this.boltId = topologyContext.getThisTaskId();
+    }
+
+    @Override
+    public void execute(final Tuple tuple) {
+        try {
+            Map doc = (Map) tuple.getValue(0);
+            EventStreamConfig eventStreamConfig = (EventStreamConfig) tuple.getValue(1);
+            System.out.println(boltId + " - Tuple Reached in the transformer bolt: " + doc);
+            List<String> configuredTransformers = eventStreamConfig.getDataTransformers();
+            if (configuredTransformers != null) {
+                String currDoc = StrawberryConfigHolder.getJsonParser().writeValueAsString(doc);
+                for (String dataTransformer : configuredTransformers) {
+                    String jsonOut = invoke(dataTransformer, currDoc);
+                    currDoc = jsonOut;
+                }
+            }
+            outputCollector.emit(tuple, new Values(doc, eventStreamConfig));
+            outputCollector.ack(tuple);
+        } catch (Exception ex) {
+            outputCollector.reportError(ex);
+        }
+    }
+
+    private String invoke(final String dataTransformer, final String jsonIn) throws Exception {
+        Class<DataTransformer> clazz = (Class<DataTransformer>) Class.forName(dataTransformer);
+        DataTransformer transformer = clazz.newInstance();
+        return transformer.transform(jsonIn);
+    }
+
+    @Override
+    public void declareOutputFields(final OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("doc", "eventStreamConfig"));
+    }
+}
